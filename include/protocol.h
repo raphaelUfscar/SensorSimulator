14b189e0d8ff33e9740b45a7ee5fbd9feadf0288
Raphael Ferreira <raphael014@outlook.com>
2026-05-10 23:57:06 +0100

    test: expand native protocol and sensor coverage (#9)

--- include/protocol.h
+++ include/protocol.h
@@ -5,5 +5,6 @@
 
 #include "types.h"
 
+bool Protocol_Encode(const SensorBatch_t* batch, uint8_t* buffer, size_t buffer_capacity, size_t* length);
 void Protocol_Encode(const SensorBatch_t* batch, uint8_t* buffer, size_t* length);
 
